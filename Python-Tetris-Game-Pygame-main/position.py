0052fd45e2f9394bc4ac059de89e0c58092c2704
Weng Jin Rui <131884797+AolaAolaAola@users.noreply.github.com>
2024-09-27 23:25:23 +0800

    Delete Python-Tetris-Game-Pygame-main directory
    duplicate

--- Python-Tetris-Game-Pygame-main/position.py
+++
@@ -1,7 +0,0 @@
-class Position:
-    def __init__(self, row, column):
-        # Initialize a Position object with row and column attributes.
-        # This class is used to represent the position of a block in the grid.
-        
-        self.row = row  # The row index of the position (vertical axis)
-        self.column = column  # The column index of the position (horizontal axis)
